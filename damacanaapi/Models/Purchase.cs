c1e2ec8099340fceca0a08c80ad53ea011b7d0a4
begum pasinli <bgm8518@gmail.com>
2015-12-18 16:59:41 +0200

    ModelsIsCreate

---
+++ damacanaapi/Models/Purchase.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace damacanaapi.Models
+{
+    public class Purchase
+    {
+        public int Id { get; set; }
+
+
+
+        public DateTime CreatedOn { get; set; }
+
+        public decimal TotalPrice { get; set; }
+
+   
+
+
+    }
+
+}
